96286ab12e80bfc683ff9980efca64463d107c6f
Aakanksha <mail2aakanksha17@gmail.com>
2019-02-25 01:04:19 +0530

    Delete Home.test.js

--- src/components/Home/Home.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
-
-it('Home renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
-});
