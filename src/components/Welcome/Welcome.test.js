2d62a7d8dade79271ef785f3f2ae0a03e1440552
Aakanksha <mail2aakanksha17@gmail.com>
2019-02-25 01:04:52 +0530

    Delete Welcome.test.js

--- src/components/Welcome/Welcome.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Welcome from './Welcome';
-
-it('Welcome renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Welcome />, div);
-});
